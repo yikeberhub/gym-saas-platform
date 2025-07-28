ffd3513fcf85ae5483e635c89d30f86ae09f2954
Yikeber <yikeber50@gmail.com>
2025-07-28 23:16:17 +0300

    routes/index.js main route added

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import authRoutes from './authRoutes';
+import userRoutes from './userRoutes';
+import gymInfoRoutes from './gymInfoRoutes';
+import memberSubscriptionRoutes from './memberSubscriptionRoutes';
+import attendanceRoutes from './attendanceRoutes';
+import notificationRoutes from './notificationRoutes';
+import paymentRoutes from './paymentRoutes';
+
+const router = Router();
+
+router.use('/auth',authRoutes);
+router.use('/user',userRoutes);
+router.use('/gymInfo',gymInfoRoutes);
+router.use('/memberSubscription',memberSubscriptionRoutes);
+router.use('/attendance',attendanceRoutes);
+router.use('/notification',notificationRoutes);
+router.use('/payment',paymentRoutes);
+
+export default router;
+
